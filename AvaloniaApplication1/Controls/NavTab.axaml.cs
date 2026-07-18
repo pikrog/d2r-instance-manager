5fb85620c61ffd03b88f2d4bfc80062ffb6d335d
pikrog <piotrekkrogulski@gmail.com>
2026-07-18 19:16:34 +0200

    add NavTab control, replace radio buttons in main window with NavTab

---
+++ AvaloniaApplication1/Controls/NavTab.axaml.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Media;
+
+namespace AvaloniaApplication1.Controls;
+
+[PseudoClasses(ActivePseudoClass)]
+public class NavTab : Button
+{
+    private const string ActivePseudoClass = ":active";
+    
+    public static readonly StyledProperty<IImage?> IconProperty = 
+        AvaloniaProperty.Register<NavTab, IImage?>(nameof(Icon));
+    
+    public static readonly StyledProperty<bool> IsActiveProperty =
+        AvaloniaProperty.Register<NavTab, bool>(nameof(IsActive));
+
+    public IImage? Icon
+    {
+        get => GetValue(IconProperty); 
+        set => SetValue(IconProperty, value);
+    }
+
+    public bool IsActive
+    {
+        get => GetValue(IsActiveProperty);
+        set => SetValue(IsActiveProperty, value);
+    }
+
+    static NavTab()
+    {
+        IsActiveProperty.Changed.AddClassHandler<NavTab>((c, e) =>
+        {
+            c.PseudoClasses.Set(ActivePseudoClass, e.NewValue is true);
+        });
+    }
+}
