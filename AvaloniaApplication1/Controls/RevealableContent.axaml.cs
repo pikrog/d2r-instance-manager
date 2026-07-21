5ebbe9257a354722ef8ebbf5d4b7f597db7a1697
pikrog <piotrekkrogulski@gmail.com>
2026-07-21 18:37:39 +0200

    remake revealable content management

---
+++ AvaloniaApplication1/Controls/RevealableContent.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaApplication1.Controls;
+
+[PseudoClasses(RevealedPseudoClass)]
+public class RevealableContent : ContentControl
+{
+    private const string RevealedPseudoClass = ":revealed";
+    
+    public static readonly StyledProperty<bool> IsRevealedProperty =
+        AvaloniaProperty.Register<RevealableContent, bool>(nameof(IsRevealed));
+    
+    public bool IsRevealed
+    {
+        get => GetValue(IsRevealedProperty);
+        set => SetValue(IsRevealedProperty, value);
+    }
+
+    static RevealableContent()
+    {
+        IsRevealedProperty.Changed.AddClassHandler<RevealableContent>((c, e) =>
+        {
+            c.PseudoClasses.Set(RevealedPseudoClass, e.NewValue is true);
+        });
+    }
+}
