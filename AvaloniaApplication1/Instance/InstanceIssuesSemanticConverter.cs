d81a0782e0d831b151d1e72a2479b32249f11014
pikrog <piotrekkrogulski@gmail.com>
2026-08-02 16:06:32 +0200

    add severity icons for instance issues in tooltip

---
+++ AvaloniaApplication1/Instance/InstanceIssuesSemanticConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using AvaloniaApplication1.Instance.ViewModels;
+using AvaloniaApplication1.Semantic;
+
+namespace AvaloniaApplication1.Instance;
+
+public class InstanceIssuesSemanticConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not InstanceItemViewModel instance || instance.IsActive || !instance.HasIssues)
+            return AvaloniaProperty.UnsetValue;
+        
+        return instance.RequiresAttention ? SemanticType.Danger : SemanticType.Warning;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AvaloniaApplication1/Instance/Views/InstanceIssueRowView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplication1.Instance.Views;
+
+public partial class InstanceIssueRowView : UserControl
+{
+    public InstanceIssueRowView()
+    {
+        InitializeComponent();
+    }
+}
