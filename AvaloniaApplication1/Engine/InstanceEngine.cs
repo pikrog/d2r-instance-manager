11a06cd557b8e99641d92f06652b8f66d90f13fc
pikrog <piotrekkrogulski@gmail.com>
2026-08-14 23:07:53 +0200

    revert to previous shutdown logic

--- AvaloniaApplication1/Engine/InstanceEngine.cs
+++ AvaloniaApplication1/Engine/InstanceEngine.cs
@@ -103,12 +103,10 @@ public class InstanceEngine : IAsyncDisposable
 
     public async Task ShutdownAsync()
     {
-        await _sessionCancellationTokenSource.CancelAsync();
-        await Task.WhenAll(_sessionTasks);
-        
         await _engineCancellationTokenSource.CancelAsync();
         await _loopTask;
-        
+        await _sessionCancellationTokenSource.CancelAsync();
+        await Task.WhenAll(_sessionTasks);
         _session.Dispose();
         _sessionCancellationTokenSource.Dispose();
         _engineCancellationTokenSource.Dispose();
